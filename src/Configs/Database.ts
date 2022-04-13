3c6a79d80c0b3b6de2270f66db1f47c70ca1a087
alqory <teamhendri18@gmail.com>
2022-04-13 20:43:23 +0700

    init 8

--- src/Configs/Database.ts
+++ src/Configs/Database.ts
@@ -17,7 +17,7 @@ config();
 // })
 
 // Deployment setting
-export const posDB = new Sequelize(`${process.env.DATABASE_URL}`,{
+export const posDB = new Sequelize('postgres://cvytsjvhvalani:c971554fbabdedc5001eefc0c50b04e7d760174a69970002927e1b6f416ed4b3@ec2-34-207-12-160.compute-1.amazonaws.com:5432/d4uucgtu5ved42',{
     dialectOptions : {
         ssl : {
             require : true,
@@ -25,6 +25,8 @@ export const posDB = new Sequelize(`${process.env.DATABASE_URL}`,{
         }
     }
 })
+
+
 
 export const dbAuthenticate = async (): Promise<void> => {
     try {
